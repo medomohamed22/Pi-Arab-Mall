db7cf79d64a7568b6cda95ded8d8ebcdfbd08786
medomohamed22 <123499261+medomohamed22@users.noreply.github.com>
2025-09-09 15:49:03 +0300

    Update complete-payment.js

--- api/complete-payment.js
+++ api/complete-payment.js
@@ -2,8 +2,7 @@ export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
 
   const { paymentId, txid } = req.body;
-
-  const r = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/complete`, {
+  await fetch(`https://api.minepi.com/v2/payments/${paymentId}/complete`, {
     method: "POST",
     headers: {
       "Authorization": `Key ${process.env.PI_API_KEY}`,
@@ -12,6 +11,5 @@ export default async function handler(req, res) {
     body: JSON.stringify({ txid })
   });
 
-  const data = await r.json();
-  res.status(200).json(data);
+  res.status(200).json({ ok: true });
 }
