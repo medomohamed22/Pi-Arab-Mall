81a7daf556a0a19b2c24a18e994f71af8ab44d9a
medomohamed22 <123499261+medomohamed22@users.noreply.github.com>
2026-07-07 17:25:38 +0300

    Update admin-config.js

--- js/admin-config.js
+++ js/admin-config.js
@@ -1,3 +1,3 @@
 ﻿// ضع Pi UID الخاص بالأدمن هنا. يمكن إضافة أكثر من أدمن.
 // مثال: window.ADMIN_PI_IDS = ['u123456789'];
-window.ADMIN_PI_IDS = ['4c6a8a85-c9f0-48e4-9bbe-1cce2ee2618b'];
+window.ADMIN_PI_IDS = ['442d72eb-95aa-45fa-86d0-4985691ca83c'];
